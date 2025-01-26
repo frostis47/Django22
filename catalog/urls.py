c79c262f3d3f4f5db5a8cec980648b46b3006aa8
fr0st <fr0st1s747@gmail.com>
2025-01-26 23:06:57 +0300

    homework_22

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,13 +1,8 @@
-from django.urls import path, include
+from django.urls import path
 
 from catalog import views
-from catalog.apps import CatalogConfig
-
-app_name = CatalogConfig.name
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("contacts/", views.contacts, name="contacts"),
-    path("catalog/", include('catalog.urls', namespace="catalog")),
-    path("", include('media.urls', namespace="media"))
+    path("contacts/", views.contacts, name="contacts")
 ]
