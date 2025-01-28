fb7143734ca9bf8bfc05e995aa47f027dac68769
fr0st <fr0st1s747@gmail.com>
2025-01-28 18:27:57 +0300

    homework_22

--- management/commands/add_product.py
+++ management/commands/add_product.py
@@ -3,24 +3,23 @@ from catalog.models import Product, Category
 
 
 class Command(BaseCommand):
-    help = 'заполнения базы-данных о продуктах'
+    help = 'Заполнение базы данных о продуктах'
 
     def handle(self, *args, **kwargs):
-        # Удаляем существующие записи
         Product.objects.all().delete()
 
-        сategory, _ = Category.objects.get_or_create(name='Фрукты')
+        category, _ = Category.objects.get_or_create(name='Фрукты')
 
         products = [
-            {'name': 'апельсин',"description": "оранжевый", 'сategory': сategory},
-            {"name": "яблоко", "description": "зеленое", 'сategory': сategory}
+            {'name': 'апельсин', "description": "оранжевый", 'category': category},
+            {"name": "яблоко", "description": "зеленое", 'category': category}
         ]
 
         for prod in products:
             product, created = Product.objects.get_or_create(**prod)
             if created:
-                self.stdout.write(self.style.SUCCESS(f'Successfully added student: '
+                self.stdout.write(self.style.SUCCESS(f'Successfully added product: '
                                                      f'{product.name} {product.description}'))
             else:
-                self.stdout.write(self.style.WARNING(f'Student already exists: '
+                self.stdout.write(self.style.WARNING(f'Product already exists: '
                                                      f'{product.name} {product.description}'))
