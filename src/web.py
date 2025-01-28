467d70e02c8f1d4e3374bf9f936ac375bff1eac7
fr0st <fr0st1s747@gmail.com>
2025-01-28 19:39:48 +0300

    homework_22

--- src/web.py
+++ src/web.py
@@ -23,17 +23,12 @@ class MyHandler(BaseHTTPRequestHandler):
 
 
 if __name__ == "__main__":
-    # Инициализация веб-сервера, который будет по заданным параметрах в сети
-    # принимать запросы и отправлять их на обработку специальному классу, который был описан выше
     webServer = HTTPServer((hostName, serverPort), MyHandler)
     print("Server started http://%s:%s" % (hostName, serverPort))
     try:
-        # Cтарт веб-сервера в бесконечном цикле прослушивания входящих запросов
         webServer.serve_forever()
     except KeyboardInterrupt:
-        # Корректный способ остановить сервер в консоли через сочетание клавиш Ctrl + C
         pass
 
-    # Корректная остановка веб-сервера, чтобы он освободил адрес и порт в сети, которые занимал
     webServer.server_close()
     print("Server stopped.")
