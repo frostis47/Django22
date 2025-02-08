a7c434b4ef522747c8de6a72bb6047911d2533ed
fr0st <fr0st1s747@gmail.com>
2025-02-08 14:30:02 +0300

    homework_22

--- config/settings.py
+++ config/settings.py
@@ -105,14 +105,12 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "static",]
 
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = (BASE_DIR / 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR / 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
