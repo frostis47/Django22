953095e324e0e6584f3d1ce121c1afc874738d87
fr0st <fr0st1s747@gmail.com>
2025-03-02 19:50:06 +0300

    homework_27

--- blog/forms.py
+++ blog/forms.py
@@ -1,8 +1,9 @@
 from django import forms
 from .models import Blog
+from django.core.exceptions import ValidationError
 
 
 class BlogForm(forms.Form):
     class Meta:
         model = Blog
-        fields = ['title', 'content', 'image', 'created_at', 'publication_sign', 'count_of_views']
+        exclude = ("count_of_views",)
--- users/forms.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.auth.forms import UserCreationForm
-from users.models import User
-
-
-class UserRegistrationForm(UserCreationForm):
-    class Meta:
-        model = User
-        fields = ('email', 'password1', 'password2')
